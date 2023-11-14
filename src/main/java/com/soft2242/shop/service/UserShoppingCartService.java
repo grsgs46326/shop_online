bc8f15fbf717eadf5c4f85f6d18a61bd3e474192
王小倩 <12933008+wwwxxx-qqq@user.noreply.gitee.com>
2023-11-14 11:26:34 +0800

    完成添加购物车、获取购物车列表、删除购物车、修改购物车、全选/取消全选购 物车信息

--- src/main/java/com/soft2242/shop/service/UserShoppingCartService.java
+++ src/main/java/com/soft2242/shop/service/UserShoppingCartService.java
@@ -2,6 +2,11 @@ package com.soft2242.shop.service;
 
 import com.soft2242.shop.entity.UserShoppingCart;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.query.CartQuery;
+import com.soft2242.shop.query.EditCartQuery;
+import com.soft2242.shop.vo.CartGoodsVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +18,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface UserShoppingCartService extends IService<UserShoppingCart> {
 
+
+     CartGoodsVO addShopCart(CartQuery query);
+     List<CartGoodsVO> shopCartList(Integer userId);
+     CartGoodsVO editCart(EditCartQuery query);
+
+     void removeCartGoods(Integer userId, List<Integer> ids);
+
+  void editCartSelected(Boolean selected, Integer userId);
 }
--- src/main/java/com/soft2242/shop/service/impl/UserShoppingCartServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/UserShoppingCartServiceImpl.java
@@ -1,10 +1,21 @@
 package com.soft2242.shop.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.soft2242.shop.common.exception.ServerException;
+import com.soft2242.shop.entity.Goods;
 import com.soft2242.shop.entity.UserShoppingCart;
+import com.soft2242.shop.mapper.GoodsMapper;
 import com.soft2242.shop.mapper.UserShoppingCartMapper;
+import com.soft2242.shop.query.CartQuery;
+import com.soft2242.shop.query.EditCartQuery;
 import com.soft2242.shop.service.UserShoppingCartService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.soft2242.shop.vo.CartGoodsVO;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -15,6 +26,96 @@ import org.springframework.stereotype.Service;
  * @since 2023-11-10
  */
 @Service
+@AllArgsConstructor
 public class UserShoppingCartServiceImpl extends ServiceImpl<UserShoppingCartMapper, UserShoppingCart> implements UserShoppingCartService {
+   private  final GoodsMapper goodsMapper;
+  @Override
+  public CartGoodsVO addShopCart(CartQuery query) {
+    Goods goods=goodsMapper.selectById(query.getId());
+    if (goods ==null){
+      throw new ServerException("商品信息不存在");
+    }
+    if (query.getCount()>goods.getInventory()){
+      throw new ServerException("商品库不存在");
+    }
+    //插入购物车信息
+    UserShoppingCart userShoppingCart = new UserShoppingCart();
+    userShoppingCart.setUserId(query.getUserId());
+    userShoppingCart.setGoodsId(goods.getId());
+    userShoppingCart.setPrice(goods.getPrice());
+    userShoppingCart.setCount(query.getCount());
+    userShoppingCart.setAttrsText(query.getAttrsText());
+    userShoppingCart.setSelected(false);
+    baseMapper.insert(userShoppingCart);
+    //返回用户添加购物车信息
+    CartGoodsVO goodsVO = new CartGoodsVO();
+    goodsVO.setId(userShoppingCart.getId());
+    goodsVO.setName(goods.getName());
+    goodsVO.setAttrsText(query.getAttrsText());
+    goodsVO.setPrice(userShoppingCart.getPrice());
+    goodsVO.setNowPrice(goods.getPrice());
+    goodsVO.setSelected(userShoppingCart.getSelected());
+    goodsVO.setStock(goods.getInventory());
+    goodsVO.setCount(query.getCount());
+    goodsVO.setPicture(goods.getCover());
+    goodsVO.setDiscount(goods.getDiscount());
+    return goodsVO;
+  }
+
+  @Override
+  public List<CartGoodsVO> shopCartList(Integer userId) {
+    List<CartGoodsVO> list = baseMapper.getCartGoodsInfo(userId);
+    return list;
+  }
+  @Override
+  public CartGoodsVO editCart(EditCartQuery query){
+    UserShoppingCart userShoppingCart = baseMapper.selectById(query.getId());
+    if (userShoppingCart==null){
+      throw new ServerException("购物车信息不存在");
+
+    }
+    userShoppingCart.setCount(query.getCount());
+    userShoppingCart.setSelected(query.getSelected());
+    baseMapper.updateById(userShoppingCart);
+    Goods goods = goodsMapper.selectById(userShoppingCart.getGoodsId());
+    if (query.getCount()>goods.getInventory()){
+      throw new ServerException(goods.getName()+"库存数量不足");
+    }
+    CartGoodsVO goodsVO = new CartGoodsVO();
+    goodsVO.setId(userShoppingCart.getId());
+    goodsVO.setName(goods.getName());
+    goodsVO.setAttrsText(userShoppingCart.getAttrsText());
+    goodsVO.setPrice(userShoppingCart.getPrice());
+    goodsVO.setNowPrice(goods.getPrice());
+    goodsVO.setSelected(userShoppingCart.getSelected());
+    goodsVO.setStock(goods.getInventory());
+    goodsVO.setCount(query.getCount());
+    goodsVO.setPicture(goods.getCover());
+    goodsVO.setDiscount(goods.getDiscount());
+    return goodsVO;
+  }
+  @Override
+  public void removeCartGoods(Integer userId, List<Integer> ids){
+    //1、查询用户的购物车列表
+    List<UserShoppingCart> cartList = baseMapper.selectList(new LambdaQueryWrapper<UserShoppingCart>().eq(UserShoppingCart::getUserId,userId));
+    if (cartList.size()==0){
+      return;
+    }
+    //2、与需要删除的购物车取交集
+    List<UserShoppingCart> deleteCartList = cartList.stream().filter(item -> ids.contains(item.getId())).collect(Collectors.toList());
+    removeBatchByIds(deleteCartList);
+  }
+  @Override
+  public void editCartSelected(Boolean selected, Integer userId){
+    List<UserShoppingCart> cartList = baseMapper.selectList(new LambdaQueryWrapper<UserShoppingCart>().eq(UserShoppingCart::getUserId, userId));
+    if (cartList.size()==0){
+      return;
+
+    }
+    //2、批量修改购物车选中状态
+    cartList.stream().forEach(item ->item.setSelected(selected));
+    //3、数据库数据更新
+    saveOrUpdateBatch(cartList);
+  }
 
 }
